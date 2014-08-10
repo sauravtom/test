a8e787f352fa997fcbe9d8105f9300ee43e922c7
sauravtom <stomatrix@gmail.com>
2014-08-10 17:07:39 +0530

    foo

---
+++ server.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import json
+import os
+import requests
+import shutil
+
+import flask
+import flask.views
+app = flask.Flask(__name__)
+from flask import render_template, Response, request
+from flask import send_from_directory, redirect
+
+app.config['APK_DIR'] = 'upload'
+
+STATIC_URL_PREFIX = ""
+
+def launcher_details():
+    arr = []
+    for folder in os.listdir('upload'):
+        d ={}
+        d['launcher_name'] = "_".join(folder.split('_')[:-1])
+        d['launcher_apk'] = STATIC_URL_PREFIX + '/launcher.apk' 
+        d['launcher_version'] = folder.split('_')[-1]
+        arr.append(d)
+    return arr
+
+@app.route('/api')
+def api():
+    arr = launcher_details()
+    return json.dumps(arr)
+
+@app.route('/api/<launcher_name>')
+def api_(launcher_name):
+    arr = launcher_details()
+    d = [i for i in arr if i['launcher_name'] == launcher_name][0]
+    return json.dumps(d)
+
+@app.route('/')
+def main():
+    arr = launcher_details()
+    return flask.render_template('index.html',arr=arr)
+
+
+@app.route('/upload_apk', methods=['POST'])
+def upload_apk():
+    launcher_name = request.form['launcher_name']
+    launcher_version = request.form['launcher_version']
+    launcher_apk = request.files['launcher_apk']
+
+    directory = "%s/%s_%s/"%(app.config['APK_DIR'],launcher_name,launcher_version)
+    
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    
+    directory = directory + os.path.sep
+    fle = open(directory + 'launcher.apk', "w+")
+    fle.write(launcher_apk.read())
+    fle.close()
+    return "Launcher uploaded successfully"
+
+@app.route('/delete/<launcher_name>')
+def delete(launcher_name):
+    arr = []
+    for folder in os.listdir('upload'):
+        arr.append(folder)
+    if launcher_name not in arr:
+        return 'Launcher not found'
+
+    shutil.rmtree('upload/' + launcher_name)
+    msg = "Your launcher has been successfully removed."
+    return msg
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
+
